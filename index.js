96b304c9835de35ea5ed6b4b7524f0e9fee97124
Nutty4616 <nkosin361@gmail.com>
2022-03-29 14:15:54 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+/**
+ * STACKS DATA STRUCTURE
+ */
+
+// Array Implementation
+/* var stack = [];
+
+stack.push("Apple");
+stack.push("Google");
+stack.push("Meta");
+stack.push("Amazon");
+
+stack.pop(); */
+
+// Linked List Implemetation
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    var newNode = new Node(value);
+
+    // check if the stack is empty
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+
+  print() {
+    var arr = [];
+    var current = this.first;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# STACK IMPLEMENTATION
+
+### push() Pseudocode
+
+- This function should accept a value.
+- Create a new node using the value passed to the function.
+- If there is no head property on the list, set the head and tail to be the newly created node.
+- Otherwise set the next property on the tail to be the new node and set the tail property on the list to be newly created node.
+- Increment the length by 1.
+- Return the linked list.
+
+### pop() Pseudocode
+
+- If there are no node in the list, return undefined.
+- Loop through the list until you reach the tail.
+- Set the next property of the 2nd to last node to be null.
+- Set the tail to be 2nd to last node.
+- Decrement the length of the list by 1.
+- Return the value of the node removed.
